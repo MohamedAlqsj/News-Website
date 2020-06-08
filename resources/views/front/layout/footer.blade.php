85951e178d09291121ded7bbb3a1e18cb060fc49
MohamedAlqsj <moh9amad1@gmail.com>
2020-06-08 15:41:13 +0300

    first commit

--- resources/views/front/layout/footer.blade.php
+++ resources/views/front/layout/footer.blade.php
@@ -1,185 +1,116 @@
-<section id="subscribe_section" class="subscribe_section">
-    <div class="row">
-        <form action="#" method="post" class="form-horizontal">
-            <div class="form-group form-group-lg">
-                <label class="col-sm-6 control-label" for="formGroupInputLarge">
-                    <h1><span class="red-color">Sign up</span> for the latest news</h1>
-                </label>
-
-                <div class="col-sm-3">
-                    <input type="text" id="subscribe" name="subscribe" class="form-control input-lg">
-                </div>
-                <div class="col-sm-1">
-                    <input type="submit" value="Sign Up" class="btn btn-large pink">
-                </div>
-                <div class="col-sm-2"></div>
-            </div>
-        </form>
-    </div>
-</section>
-<!-- Subscriber Section -->
-
-<section id="footer_section" class="footer_section">
+<!-- Main Footer Area -->
+<footer class="footer-area">
+<div class="main-footer-area">
     <div class="container">
-        <hr class="footer-top">
         <div class="row">
-            <div class="col-md-3">
-                <div class="footer_widget_title"><h3><a href="category.html" target="_self">About Tech</a></h3></div>
-                <div class="logo footer-logo">
-                    <a title="fontanero" href="index.html">
-                        <img src="{{asset('front/img/tech_about.jpg')}}" alt="technews">
-                    </a>
 
-                    <p>Competently conceptualize go forward testing procedures and B2B expertise. Phosfluorescently
-                        cultivate principle-centered methods.of empowerment through fully researched.</p>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="footer_widget_title">
-                    <h3><a href="category.html" target="_self">Discover</a></h3>
-                </div>
-                <div class="row">
-                    <div class="col-xs-4">
-                        <ul class="list-unstyled left">
-                            <li><a href="#">Mobile</a></li>
-                            <li><a href="#">Tablet</a></li>
-                            <li><a href="#">Gadgets</a></li>
-                            <li><a href="#">Design</a></li>
-                            <li><a href="#">Camera</a></li>
-                            <li><a href="#">Apps</a></li>
-                            <li><a href="#">Login</a></li>
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">Membership</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">Job</a></li>
-                            <li><a href="#">SiteMap</a></li>
-                        </ul>
+            <!-- Footer Widget Area -->
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="footer-widget-area">
+                    <!-- Footer Logo -->
+                    <div class="footer-logo">
+                        <a href="index.html"><img src="img/core-img/logo.png" alt=""></a>
                     </div>
-                    <div class="col-xs-8">
-                        <ul class="list-unstyled">
-                            <li><a href="#">Contact Us</a></li>
-                            <li><a href="#">Newsletter Alerts</a></li>
-                            <li><a href="#">Podcast</a></li>
-                            <li><a href="#">Sms Subscription</a></li>
-                            <li><a href="#">Advertisement Policy</a></li>
-                            <li><a href="#">Report Technical issue</a></li>
-                            <li><a href="#">Complaints and Corrections</a></li>
-                            <li><a href="#">Terms and Conditions</a></li>
-                            <li><a href="#">Privacy Policy</a></li>
-                            <li><a href="#">Cookie Policy</a></li>
-                            <li><a href="#">Securedrop</a></li>
-                            <li><a href="#">Archives</a></li>
+                    <!-- Footer Nav -->
+                    <div class="footer-nav">
+                        <ul>
+                            <li class="active"><a href="#">Top 10</a></li>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">Funny</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Celebs</a></li>
+                            <li><a href="#">Lifestyle</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">Travel</a></li>
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">Submit a video</a></li>
+                            <li><a href="#">Don’tMiss</a></li>
+                            <li><a href="#">Contact</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
-            <div class="col-md-3">
-                <div class="footer_widget_title">
-                    <h3><a href="#" target="_self">Editor Picks</a></h3>
-                </div>
-                <div class="media">
-                    <div class="media-left">
-                        <a href="#"><img class="media-object" src="{{asset('front/img/editor_pic1.jpg')}}"
-                                         alt="Generic placeholder image"></a>
-                    </div>
-                    <div class="media-body">
-                        <h3 class="media-heading">
-                            <a href="single.html">Apple launches photo-centric wrist watch for Android</a>
-                        </h3>
-                        <span class="rating">
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star-half-full"></i>
-                        </span>
-                    </div>
-                </div>
-                <div class="media">
-                    <div class="media-left">
-                        <a href="#"><img class="media-object" src="{{asset('front/img/editor_pic2.jpg')}}"
-                                         alt="Generic placeholder image"></a>
-                    </div>
-                    <div class="media-body">
-                        <h3 class="media-heading">
-                            <a href="single.html">Apple launches photo-centric wrist watch for Android</a>
-                        </h3>
-                        <span class="rating">
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star-half-full"></i>
-                        </span>
-                    </div>
-                </div>
-                <div class="media">
-                    <div class="media-left">
-                        <a href="#"><img class="media-object" src="{{asset('front/img/editor_pic3.jpg')}}"
-                                         alt="Generic placeholder image"></a>
-                    </div>
-                    <div class="media-body">
-                        <h3 class="media-heading">
-                            <a href="single.html">Apple launches photo-centric wrist watch for Android</a>
-                        </h3>
-                        <span class="rating">
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star"></i>
-                            <i class="fa fa-star-half-full"></i>
-                        </span>
-                    </div>
+
+            <div class="col-12 col-md-6 col-lg-4">
+                <!-- Newsletter Widget -->
+                <div class="newsletter-widget">
+                    <h4>Sign up to <br>our newsletter</h4>
+                    <form action="#" method="post">
+                        <input type="text" name="text" placeholder="Name">
+                        <input type="email" name="email" placeholder="Email">
+                        <button type="submit" class="btn w-100">Subscribe</button>
+                    </form>
                 </div>
             </div>
-            <div class="col-md-3">
-                <div class="footer_widget_title">
-                    <h3><a href="category.html" target="_self">Tech Photos</a></h3>
-                </div>
-                <div class="widget_photos">
-                    <img class="img-thumbnail" src="assets/img/tech_photo1.jpg" alt="Tech Photos">
-                    <img class="img-thumbnail" src="assets/img/tech_photo2.jpg" alt="Tech Photos">
-                    <img class="img-thumbnail" src="assets/img/tech_photo3.jpg" alt="Tech Photos">
-                    <img class="img-thumbnail" src="assets/img/tech_photo4.jpg" alt="Tech Photos">
-                    <img class="img-thumbnail" src="assets/img/tech_photo5.jpg" alt="Tech Photos">
-                    <img class="img-thumbnail" src="assets/img/tech_photo6.jpg" alt="Tech Photos">
-                    <img class="img-thumbnail" src="assets/img/tech_photo7.jpg" alt="Tech Photos">
-                    <img class="img-thumbnail" src="assets/img/tech_photo8.jpg" alt="Tech Photos">
-                    <img class="img-thumbnail" src="assets/img/tech_photo9.jpg" alt="Tech Photos">
-                    <img class="img-thumbnail" src="assets/img/tech_photo10.jpg" alt="Tech Photos">
-                    <img class="img-thumbnail" src="assets/img/tech_photo11.jpg" alt="Tech Photos">
-                    <img class="img-thumbnail" src="assets/img/tech_photo12.jpg" alt="Tech Photos">
-                </div>
 
-            </div>
-        </div>
-    </div>
+            <!-- Footer Widget Area -->
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="footer-widget-area">
+                    <!-- Widget Title -->
+                    <h4 class="widget-title">Latest articles</h4>
 
-    <div class="footer_bottom_Section">
-        <div class="container">
-            <div class="row">
-                <div class="footer">
-                    <div class="col-sm-3">
-                        <div class="social">
-                            <a class="icons-sm fb-ic"><i class="fa fa-facebook"></i></a>
-                            <!--Twitter-->
-                            <a class="icons-sm tw-ic"><i class="fa fa-twitter"></i></a>
-                            <!--Google +-->
-                            <a class="icons-sm inst-ic"><i class="fa fa-instagram"> </i></a>
-                            <!--Linkedin-->
-                            <a class="icons-sm tmb-ic"><i class="fa fa-tumblr"> </i></a>
-                            <!--Pinterest-->
-                            <a class="icons-sm rss-ic"><i class="fa fa-rss"> </i></a>
+                    <!-- Single Latest Post -->
+                    <div class="single-blog-post style-2 d-flex align-items-center">
+                        <div class="post-thumb">
+                            <a href="#"><img src="img/bg-img/4.jpg" alt=""></a>
+                        </div>
+                        <div class="post-data">
+                            <a href="#" class="post-title">
+                                <h6>Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.</h6>
+                            </a>
+                            <div class="post-meta">
+                                <p class="post-date"><a href="#">7:00 AM | April 14</a></p>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-sm-6">
-                        <p>&copy; Copyright 2016-Tech News . Design by: <a href="https://uicookies.com">uiCookies</a> </p>
+
+                    <!-- Single Latest Post -->
+                    <div class="single-blog-post style-2 d-flex align-items-center">
+                        <div class="post-thumb">
+                            <a href="#"><img src="img/bg-img/5.jpg" alt=""></a>
+                        </div>
+                        <div class="post-data">
+                            <a href="#" class="post-title">
+                                <h6>Sed a elit euismod augue semper congue sit amet ac.</h6>
+                            </a>
+                            <div class="post-meta">
+                                <p class="post-date"><a href="#">7:00 AM | April 14</a></p>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-sm-3">
-                        <p>Technology News Magazine</p>
+
+                    <!-- Single Latest Post -->
+                    <div class="single-blog-post style-2 d-flex align-items-center">
+                        <div class="post-thumb">
+                            <a href="#"><img src="img/bg-img/6.jpg" alt=""></a>
+                        </div>
+                        <div class="post-data">
+                            <a href="#" class="post-title">
+                                <h6>Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.</h6>
+                            </a>
+                            <div class="post-meta">
+                                <p class="post-date"><a href="#">7:00 AM | April 14</a></p>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
     </div>
-</section>
+</div>
+
+<!-- Bottom Footer Area -->
+<div class="bottom-footer-area">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center">
+            <div class="col-12">
+                <!-- Copywrite -->
+                <p><a href="#"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+            </div>
+        </div>
+    </div>
+</div>
+</footer>
